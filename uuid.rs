b7986935c8e659b32d12daa86c63e7722650ffb7
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2012-03-29 08:38:36 -0700

    Make rust-uuid portable to other *nixs.

--- uuid.rs
+++ uuid.rs
@@ -1,3 +1,8 @@
+export uuid;
+export uuid_random;
+export uuid_time;
+export to_str;
+export from_str;
 
 #[doc = "a uuid value"]
 type uuid = {
@@ -7,8 +12,23 @@ type uuid = {
     d: u32,
 };
 
+#[cfg(target_os = "macos")]
 #[nolink]
-native mod libuuid {
+native mod uuid {
+    fn uuid_generate(out: uuid);
+    fn uuid_generate_random(out: uuid);
+    fn uuid_generate_time(out: uuid);
+
+    fn uuid_parse(s: *u8, uuid: uuid) -> libc::c_int;
+
+    fn uuid_unparse(uuid: uuid, out: *u8);
+    fn uuid_unparse_lower(uuid: uuid, out: *u8);
+    fn uuid_unparse_upper(uuid: uuid, out: *u8);
+}
+
+#[cfg(target_os = "linux")]
+#[cfg(target_os = "freebsd")]
+native mod uuid {
     fn uuid_generate(out: uuid);
     fn uuid_generate_random(out: uuid);
     fn uuid_generate_time(out: uuid);
@@ -21,23 +41,23 @@ native mod libuuid {
 }
 
 #[doc = "Create a new uuid"]
-fn mk_uuid() -> uuid {
+fn uuid() -> uuid {
     let uuid = { a: 0u32, b: 0u32, c: 0u32, d: 0u32 };
-    libuuid::uuid_generate(uuid);
+    uuid::uuid_generate(uuid);
     uuid
 }
 
 #[doc = "Create a uuid from the current time and mac address"]
-fn mk_uuid_random() -> uuid {
+fn uuid_random() -> uuid {
     let uuid = { a: 0u32, b: 0u32, c: 0u32, d: 0u32 };
-    libuuid::uuid_generate_random(uuid);
+    uuid::uuid_generate_random(uuid);
     uuid
 }
 
 #[doc = "Create a uuid from a random number generator"]
-fn mk_uuid_time() -> uuid {
+fn uuid_time() -> uuid {
     let uuid = { a: 0u32, b: 0u32, c: 0u32, d: 0u32 };
-    libuuid::uuid_generate_time(uuid);
+    uuid::uuid_generate_time(uuid);
     uuid
 }
 
@@ -47,7 +67,7 @@ fn to_str(uuid: uuid) -> str {
     str::reserve(s, 36u);
     unsafe { str::unsafe::set_len(s, 36u); }
     str::as_buf(s) { |buf|
-        libuuid::uuid_unparse(uuid, buf);
+        uuid::uuid_unparse(uuid, buf);
     }
     s
 }
@@ -58,7 +78,7 @@ fn from_str(s: str) -> uuid {
 
     let uuid = { a: 0u32, b: 0u32, c: 0u32, d: 0u32 }; 
     str::as_buf(s) { |buf|
-        libuuid::uuid_parse(buf, uuid);
+        uuid::uuid_parse(buf, uuid);
     }
     uuid
 }
@@ -68,13 +88,13 @@ mod test {
     #[test]
     fn test() {
         uint::range(0u, 100000u) { |_i|
-            let uuid = mk_uuid();
+            let uuid = uuid();
             assert uuid == from_str(to_str(uuid));
 
-            let uuid = mk_uuid_random();
+            let uuid = uuid_random();
             assert uuid == from_str(to_str(uuid));
 
-            let uuid = mk_uuid_time();
+            let uuid = uuid_time();
             assert uuid == from_str(to_str(uuid));
         }
     }
